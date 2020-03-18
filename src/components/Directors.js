02a661521a10b73d9686f9bc19a9c8c40a4babca
Mari <demarius.edwards@gmail.com>
2020-03-18 18:35:25 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
+const Directors = (props) => {
+  props = directors
+  const renderDirectors = () => {
+  return props.map(obj => 
+    <div>
+      <h1>Name: {obj.name}</h1>
+        <ul>
+  Movies:<ul>{obj.movies.map(movie =>  { return <li>{movie}</li>})} </ul>
+        </ul>
+    
+    </div>
+    )
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { movies } from '../data';
+import { render } from 'enzyme';
 
-const Movies = () => {
+const Movies = (props) => {
+  props = movies
+const renderMovies = () => {
+    return props.map(movieObj => 
+    <div>
+      <h1>Title: {movieObj.title}</h1>
+    <p>Time: {movieObj.time} Mins</p>
+    <p>Rating: {movieObj.metascore}</p>
+      <ul> Genres: 
+        {movieObj.genres.map(genre => 
+        <ul> 
+          <li>{genre}</li>
+
+        </ul>)}
+      </ul>
+
+    </div>
+    )
+  }
+  console.log(props)
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       {renderMovies()}
     </div>
   );
 };
